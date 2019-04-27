44b6d16fda54cb124502ea5b5b2641cfffb38f94
phdmf <phdmf@cin.ufpe.br>
2019-04-27 08:37:20 -0300

    Aplicando funções básicas de uma calculadora

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,3 +1,27 @@
 function calcular(tipo, valor) { 
-	console.log(tipo, valor)
+	
+	// verifico se o tipo é uma ação ou um valor
+	if(tipo === 'acao'){
+
+		if(valor === 'clear'){ // se o 'C' for pressionado limpo o visor
+			document.getElementById('resultado').value = ''
+		}
+		else if (valor !== '='){ // se for qualquer uma das operações ou o 'ponto'
+
+			document.getElementById('resultado').value += valor
+
+		}else{
+			//efetuo a operação utilizando o 'eval' e jogo esse resultado de da variavel de memso nome
+			var resultado = eval(document.getElementById('resultado').value)
+			
+			//acesso o elemnto e alteo seu valor pro resultado da operacao
+			document.getElementById('resultado').value = resultado
+		}
+
+
+	}else if(tipo === 'valor'){
+
+		document.getElementById('resultado').value += valor // concatenação dos valores recebidos por parametro
+	}
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Calculadora
-Uma simples calculadora feita com Javascript
+
+### Uma simples calculadora feita com Javascript
+
+Até o momento é possível executar as funções mais simples de uma calculadora. 
+Além disso, são aceitas apenas operações matemáticas válidas, ou seja, operações como:
+_4 +++++ 2_ **não são aceitas.** Vale lembrar também que, após cada clique no '=' é preciso limpar a calculadora para uma nova operação. 
